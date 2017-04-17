d4fbde31202d5b7867792dc43e4c308c5fbc7431
Nikita Allin <nikita.allin@swisstok.ru>
2017-04-17 22:17:13 +0300

    Add intergration json config

--- features/Factory.ts
+++ features/Factory.ts
@@ -1,12 +1,20 @@
 import {FeatureSQLStorage} from "./Storages";
 import {FeatureRepository} from "./Repositories";
 import * as pg from "pg";
+import {Value, Properties} from 'ts-json-properties';
 
 class FeatureFactory {
     private _rep;
 
+    @Value('config.db')
+    private dbJson:any;
+
     constructor() {
-        let config = require("/config/db.json");
+        let config = Properties.getValue('db');
+
+        console.log(config);
+
+
         this._rep = new FeatureRepository([new FeatureSQLStorage(new pg.Pool(config.dev))]);
     }
 
