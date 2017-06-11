462a31adb61f1ca76de8fe3d64ba6aaca9de3f8c
GolemXIV <Shaper2010@yandex.ru>
2017-06-11 15:10:45 +0400

    callback auth problem

--- auth/Auth.ts
+++ auth/Auth.ts
@@ -5,13 +5,12 @@ import * as jwt from "jsonwebtoken";
 import * as bcrypt from "bcrypt";
 import {Value} from "ts-json-properties";
 import {Observable} from "rxjs/Observable";
-import Rx from 'rxjs/Rx';
 
 export class AuthService {
     @Value("config.settings")
     private settings: any;
 
-    public createToken(email: string): Observable<string> {
+    public createToken(email: string): Promise<string> {
         let payload = {email: email};
         let callback = (err, decoded) => {
             if (err) {
@@ -21,18 +20,13 @@ export class AuthService {
             }
         };
         jwt.sign(payload, this.settings.key, {expiresIn: 60 * 60}, callback);
-        Rx.Observable.range(1, 3)
-            .map(function (x, idx, obs) {
-                return x * x;
-            });
-
-        return Observable.bindCallback(callback);
+        return new Promise(callback);
     }
 
-    public verifyToken(token: string): Observable<boolean> {
+    public verifyToken(token: string): Promise<boolean> {
         let callback = (err, decoded) => !err || decoded;
         jwt.verify(token, this.settings.key, callback);
-        return Observable.bindCallback(callback);
+        return new Promise(callback);
     }
 
     public createHash(password: string): Observable<string> {
