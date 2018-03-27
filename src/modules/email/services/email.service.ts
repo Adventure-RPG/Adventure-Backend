3224374f20de0232c6877f3bf74680bdcf978198
golemxlv <shaper2010@yandex.ru>
2018-03-27 21:35:08 +0400

    improve mail logic

--- src/modules/email/services/email.service.ts
+++ src/modules/email/services/email.service.ts
@@ -1,5 +1,6 @@
 import {Component, Inject, LoggerService} from '@nestjs/common';
 import * as nodemailer from 'nodemailer';
+import {SentMessageInfo} from 'nodemailer';
 import {Config} from '../../../app.util';
 import * as Mail from 'nodemailer/lib/mailer';
 import {APP_LOGGER_TOKEN} from '../../../constants/app.constants';
@@ -14,15 +15,11 @@ export class EmailService {
 
   @Config('email')
   private _config;
+  private _connectedSuccessful: boolean = false;
   private _mailer: Mail;
 
   constructor(@Inject(APP_LOGGER_TOKEN) private _logger: LoggerService) {
-    this._mailer = nodemailer.createTransport(this._config.smtp.options, this._config.smtp.defaults);
-
-    // TODO: make a waiting function for mail service
-    this._mailer.verify((err) => {
-        if (err) this._logger.warn(err.message);
-    });
+    this.connect();
   }
 
   sendVerificationEmail(user: UserDto, verify: EmailVerifyDto) {
@@ -30,7 +27,30 @@ export class EmailService {
     const html = pug.renderFile(this._config.templateDir + 'body/html/' + this._config.templates.verify, {name: user.email, link: this.generateVerifyLink(verify.token), project: this._global.project});
     const plain = pug.renderFile(this._config.templateDir + 'body/plain/' + this._config.templates.verify, {name: user.email, link: this.generateVerifyLink(verify.token), project: this._global.project});
 
-    this._mailer.sendMail({to: user.email, subject: title, html, text: plain});
+    this.sendMail(user.email, title, html, plain);
+  }
+
+  async connect(): Promise<boolean> {
+    this._mailer = nodemailer.createTransport(this._config.smtp.options, this._config.smtp.defaults);
+
+    return new Promise<boolean>((resolve, reject) => {
+        this._mailer.verify((err) => {
+            if (err) {
+                this._logger.warn(err.message);
+                reject(err);
+                this._connectedSuccessful = false;
+            } else {
+              this._connectedSuccessful = true;
+              resolve(true);
+            }
+        });
+    });
+  }
+
+  async sendMail(to, subject, html, text): Promise<SentMessageInfo> {
+    if (!this._connectedSuccessful)
+      await this.connect();
+    return this._mailer.sendMail({to, subject, html, text});
   }
 
   generateVerifyLink(token: string) {
